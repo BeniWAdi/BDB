ce6f99bb891baf6e95c3381a2064fe7528012940
BeniWAdi <30317527+BeniWAdi@users.noreply.github.com>
2019-10-10 16:06:34 +0700

    Add files via upload

---
+++ BDB/BConfig.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * BDB is ...
+ * 
+ * @author Beni Wahyu Adi <beniwahyuadi@gmail.com>
+ * @version 0.02
+ */
+
+class BConfig {
+
+    protected static $servername = "localhost";
+    protected static $username = "root";
+    protected static $password = "";
+    protected static $dbname = "";
+    protected static $charset = "utf8";
+    
+    protected static $showError = true;
+    protected static $showSqlWhenError = true;
+    protected static $killAppWhenError = true;
+
+}
